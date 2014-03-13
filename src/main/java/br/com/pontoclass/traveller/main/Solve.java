6a2133e69bb34ee9e11394fa36d850d9e3e69d76
cad_rpereira <cad_rpereira@uolinc.com>
2014-03-13 20:03:10 -0300

    BROKEN:Incrementing the .gitignore and adding TDD code.

--- src/main/java/br/com/pontoclass/traveller/main/Solve.java
+++ src/main/java/br/com/pontoclass/traveller/main/Solve.java
@@ -2,6 +2,21 @@ package br.com.pontoclass.traveller.main;
 
 public class Solve {
 	public static void main(String[] args) {
-		//TODO: add code here
+		Solver solver = SolverBuilder.createBuilder().withMapping(args).build();
+		boolean fail = false;
+		try {
+			Answer answer = solver.solve();
+		} catch (TravellerSalesmanException e) {
+			String explaination = e.getExplaination();
+			fail = true;
+		}
+		if(fail) {
+			
+		} else {
+			Route best = answer.getBestRoute();
+			String bestWayString = best.getWayString();
+			double totalValue = best.getTotalWayCost();
+			Route[] tried = answer.getTriedRoutes();
+		}
 	}
 }
